2c221da8d1c5ca67f491c336f85ad2e1aca504ef
Danish Ikbal <mdcse786@gmail.com>
2022-09-02 01:18:49 +0530

    fixed Overflow

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -51,171 +51,173 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Expanded(
                 flex: 2,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Mybutton(
-                          title: 'AC',
-                          onpress: () {
-                            userInput = '';
-                            answer = '';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '+/-',
-                          onpress: () {
-                            userInput += '+/-';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '%',
-                          onpress: () {
-                            userInput += '%';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                            title: '/',
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Mybutton(
+                            title: 'AC',
                             onpress: () {
-                              userInput += '/';
+                              userInput = '';
+                              answer = '';
                               setState(() {});
                             },
-                            buttonClr: orrangeColor),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Mybutton(
-                          title: '7',
-                          onpress: () {
-                            userInput += '7';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '8',
-                          onpress: () {
-                            userInput += '8';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '9',
-                          onpress: () {
-                            userInput += '9';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                            title: 'x',
+                          ),
+                          Mybutton(
+                            title: '+/-',
                             onpress: () {
-                              userInput += 'x';
+                              userInput += '+/-';
                               setState(() {});
                             },
-                            buttonClr: orrangeColor),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Mybutton(
-                          title: '4',
-                          onpress: () {
-                            userInput += '4';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '5',
-                          onpress: () {
-                            userInput += '5';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '6',
-                          onpress: () {
-                            userInput += '6';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                            title: '-',
+                          ),
+                          Mybutton(
+                            title: '%',
                             onpress: () {
-                              userInput += '-';
+                              userInput += '%';
                               setState(() {});
                             },
-                            buttonClr: orrangeColor),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Mybutton(
-                          title: '1',
-                          onpress: () {
-                            userInput += '1';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '2',
-                          onpress: () {
-                            userInput += '2';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '3',
-                          onpress: () {
-                            userInput += '3';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                            title: '+',
+                          ),
+                          Mybutton(
+                              title: '/',
+                              onpress: () {
+                                userInput += '/';
+                                setState(() {});
+                              },
+                              buttonClr: orrangeColor),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Mybutton(
+                            title: '7',
                             onpress: () {
-                              userInput += '+';
+                              userInput += '7';
                               setState(() {});
                             },
-                            buttonClr: orrangeColor),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Mybutton(
-                          title: '0',
-                          onpress: () {
-                            userInput += '0';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: '.',
-                          onpress: () {
-                            userInput += '.';
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                          title: 'DEL',
-                          onpress: () {
-                            userInput =
-                                userInput.substring(0, userInput.length - 1);
-                            setState(() {});
-                          },
-                        ),
-                        Mybutton(
-                            title: '=',
+                          ),
+                          Mybutton(
+                            title: '8',
                             onpress: () {
-                              equalpress();
+                              userInput += '8';
                               setState(() {});
                             },
-                            buttonClr: orrangeColor),
-                      ],
-                    ),
-                  ],
+                          ),
+                          Mybutton(
+                            title: '9',
+                            onpress: () {
+                              userInput += '9';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                              title: 'x',
+                              onpress: () {
+                                userInput += 'x';
+                                setState(() {});
+                              },
+                              buttonClr: orrangeColor),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Mybutton(
+                            title: '4',
+                            onpress: () {
+                              userInput += '4';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: '5',
+                            onpress: () {
+                              userInput += '5';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: '6',
+                            onpress: () {
+                              userInput += '6';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                              title: '-',
+                              onpress: () {
+                                userInput += '-';
+                                setState(() {});
+                              },
+                              buttonClr: orrangeColor),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Mybutton(
+                            title: '1',
+                            onpress: () {
+                              userInput += '1';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: '2',
+                            onpress: () {
+                              userInput += '2';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: '3',
+                            onpress: () {
+                              userInput += '3';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                              title: '+',
+                              onpress: () {
+                                userInput += '+';
+                                setState(() {});
+                              },
+                              buttonClr: orrangeColor),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Mybutton(
+                            title: '0',
+                            onpress: () {
+                              userInput += '0';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: '.',
+                            onpress: () {
+                              userInput += '.';
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                            title: 'DEL',
+                            onpress: () {
+                              userInput =
+                                  userInput.substring(0, userInput.length - 1);
+                              setState(() {});
+                            },
+                          ),
+                          Mybutton(
+                              title: '=',
+                              onpress: () {
+                                equalpress();
+                                setState(() {});
+                              },
+                              buttonClr: orrangeColor),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
